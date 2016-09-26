7a4f932bc12b47ccaf4f3369d212c2e8984a19ae
Julian Chung <juliancwt77@gmail.com>
2016-09-26 15:44:11 +0800

    updates

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', init)
+
+function init () {
+
+var buttonYellow = document.getElementById('button-yellow')
+
+buttonYellow.addEventListener('click', joint)
+
+function joint () {
+
+prompt('Give me your email!')
+
+
+}
+
+}
